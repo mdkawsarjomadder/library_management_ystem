118121051c472d24d42c48c5751e67da04c38bb4
md kawsar <mdkawsar342814@gmail.com>
2026-07-09 01:41:23 +0600

    Books in Author and Publisher create

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using LibraryManagementSystem.Data;
 using LibraryManagementSystem.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagementSystem.Controllers
@@ -12,40 +9,52 @@ namespace LibraryManagementSystem.Controllers
     public class BooksController : Controller
     {
         private readonly AppDbContext _context;
+
         public BooksController(AppDbContext context)
         {
             _context = context;
         }
+
+        // GET: Books
         public async Task<IActionResult> Index()
         {
             var books = await _context.Books
-            .Include(x =>x.Category)
-            .Include(x =>x.Author)
-            .Include(x => x.Publisher)
-            .ToListAsync();
+                .Include(x => x.Category)
+                .Include(x => x.Author)
+                .Include(x => x.Publisher)
+                .ToListAsync();
+
             return View(books);
         }
 
-      // Create GET Method.......  {GET: Books/Create}
+        // GET: Books/Create
         public IActionResult Create()
         {
-            return View();
+        ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+        ViewBag.Authors = new SelectList(_context.Authors, "Id", "Name");
+        ViewBag.Publishers = new SelectList(_context.Publishers, "Id", "Name");
+
+        return View();
         }
 
-        //Create Post Method -----------{post: Books/create}
+        // POST: Books/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(Book book)
         {
-            if(ModelState.IsValid)
-            {
-                _context.Books.Add(book);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));               
-            }
-             return View(book);
-        }    
+        if (ModelState.IsValid)
+        {
+        _context.Books.Add(book);
+        await _context.SaveChangesAsync();
 
+        return RedirectToAction(nameof(Index));
+        }
 
-    }
-}
+        ViewBag.Categories = new SelectList(_context.Categories, "Id", "Name");
+        ViewBag.Authors = new SelectList(_context.Authors, "Id", "Name");
+        ViewBag.Publishers = new SelectList(_context.Publishers, "Id", "Name");
+
+        return View(book);
+        }
+        }
+        }
