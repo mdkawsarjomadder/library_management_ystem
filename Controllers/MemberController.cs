f71dcc633ec5ef4941bf27c4aa721c4ffaaa57cc
md kawsar <mdkawsar342814@gmail.com>
2026-07-10 14:44:39 +0600

    Member_edit

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -2,11 +2,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryManagementSystem.Controllers
 {
-    public class MemberController
+  public class MembersController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public MembersController(AppDbContext context)
     {
-        
+        _context = context;
     }
-}
+
+    public async Task<IActionResult> Index()
+    {
+        var members = await _context.Members.ToListAsync();
+        return View(members);
+    }
+
+
+        // GET: Members/Create
+    public IActionResult Create()
+    {
+        return View();
+        }
+    
+
+      // POST: Members/Create
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+       public async Task<IActionResult> Create(Member member)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Members.Add(member);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(member);
+        }
+
+        //Get member/Edit/1
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+           var member = await _context.Members.FindAsync(id);
+           if(member == null)
+            {
+                return NotFound();
+            }
+            return View(member);
+        }
+
+
+       } // MembersController : Controller    
+    }// LibraryManagementSystem : Controllers
--- obj/Debug/net10.0/LibraryManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/LibraryManagementSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48442bdd8701c4ee997b2936a05f0a22164ca2e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+527bc6e3ba202824f253b0d4f354750f67b2e4e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
